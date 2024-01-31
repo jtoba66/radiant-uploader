3f9440254fc71a105f9f9abd7968815dbd8f07a1
Nugget <nuggetnchill@gmail.com>
2024-01-31 15:26:10 +0700

    file icon based on type

--- src/components/EachFile.js
+++ src/components/EachFile.js
@@ -2,13 +2,36 @@ import React from "react"
 import { truncate } from "../utils"
 
 import copy_icon from "../assets/copy_icon.png"
-import file_icon from "../assets/file.png"
+import fallback from "../assets/file-icons/fallback.png"
+import jpeg from "../assets/file-icons/jpeg.png"
+import pdf from "../assets/file-icons/pdf.png"
+import png from "../assets/file-icons/png.png"
+import text from "../assets/file-icons/text.png"
+import mp3 from "../assets/file-icons/mp3.png"
+import mp4 from "../assets/file-icons/mp4.png"
+
+const IconCollection = {
+	png,
+	pdf,
+	jpeg,
+	jpg: jpeg,
+	docx: text,
+	txt: text,
+	mp4,
+	mp3
+}
 
 export default function EachFile({ file, copyToClipboard, openFile }) {
+	let fileType = file.name.split(".")[1]
+
 	return (
 		<div className='each-file'>
 			<div className='file-name'>
-				<img height={30} alt='file_icon' src={file_icon} />
+				<img
+					height={50}
+					alt='file icon'
+					src={IconCollection[fileType] || fallback}
+				/>
 				{truncate(file.name, 30)}
 			</div>
 			<p
